ce70958d87773f5ef1c48df0eb759df6162209ab
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-11 17:15:24 +0000

    Add filter, search, display and prepopulate slug functions to admin panel

--- recipe/admin.py
+++ recipe/admin.py
@@ -5,4 +5,14 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    summernote_fields = ('introduction', 'ingredients', 'steps')
+
+    list_filter = (
+        'status', 'created_on', 'type', 'author', 'category')
+    list_display = (
+        'title', 'author', 'slug', 'status', 'created_on', 'type', 'category')
+    search_fields = [
+        'title', 'introduction', 'ingredients']
+    prepopulated_fields = {
+        'slug': ('title',)}
+    summernote_fields = (
+        'introduction', 'ingredients', 'steps')
