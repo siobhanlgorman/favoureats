7b4f6e7ba3c43e176b6b307c4519e8877cabfaac
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-12 15:12:23 +0000

    Add code to display test posts on index.html

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import generic
-
 from .models import Recipe
 
 class HomeList(generic.ListView):
@@ -9,9 +8,8 @@ class HomeList(generic.ListView):
     template_name = 'index.html'
 
 
-
-# class RecipeList(generic.ListView):
-#     model = Recipe
-#     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-#     template_name = 'recipes.html'
-#     paginate_by = 6
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'recipes.html'
+    paginate_by = 6
