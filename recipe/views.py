49bf1f3ba5ff967e8f3e54fb69649f9a11551e11
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-12 10:29:41 +0000

    Add view code for index.html

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Recipe
+
+class HomeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')[0:3]
+    template_name = 'index.html'
+
+
+
+# class RecipeList(generic.ListView):
+#     model = Recipe
+#     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+#     template_name = 'recipes.html'
+#     paginate_by = 6
