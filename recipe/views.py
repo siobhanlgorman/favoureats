b5e15515366f396488ad76509e8b7e08b9e3ff79
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-15 07:26:58 +0000

    Add post method to review form

--- recipe/views.py
+++ recipe/views.py
@@ -39,8 +39,42 @@ class RecipeDetail(View):
             {
                 "recipe": recipe,
                 "reviews": reviews,
+                "reviewed": False,
                 "favourites": favourites,
                 "review_form": ReviewForm()
 
             },
         )
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        reviews = recipe.reviews.filter(approved=True).order_by('created_on')
+        # check if logged in user has liked this recipe
+        favourites = False
+        if recipe.favourites.filter(id=self.request.user.id).exists():
+            favourites = True
+
+        review_form = ReviewForm(data=request.POST)
+
+        if review_form.is_valid():
+            review_form.instance.email = request.user.email
+            review_form.instance.name = request.user.username
+            review = review_form.save(commit=False)
+            review.recipe = recipe
+            review.save()
+        else:
+            review_form = ReviewForm()
+
+
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                "recipe": recipe,
+                "reviews": reviews,
+                "reviewed": True,
+                "favourites": favourites,
+                "review_form": ReviewForm()
+
+            },
+        ) 
