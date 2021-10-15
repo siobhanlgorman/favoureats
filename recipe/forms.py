6ed7c009d7dff0ca4d2c259b80dcb4fae0b63d1b
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-15 15:55:47 +0000

    Create recipe form

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Review
+from .models import Review, Recipe
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ('title', 'ingredients', 'steps', 'servings', 'cooktime_hours', 'cooktime_mins', 'recipe_image')
