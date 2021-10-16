68d448856ae378f87617907fd6d7f4b801683984
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-16 16:40:32 +0000

    Update recipe view to include author

--- recipe/views.py
+++ recipe/views.py
@@ -102,6 +102,6 @@ class MyRecipeList(generic.ListView):
 
 class RecipeCreate(generic.CreateView):
     model = Recipe
-    fields = ['title', 'ingredients', 'steps', 'servings', 'cooktime_hours', 'cooktime_mins', 'type', 'category', ]
+    fields = ['author', 'title', 'ingredients', 'steps', 'servings', 'cooktime_hours', 'cooktime_mins', 'type', 'category', ]
     template_name = 'recipe_form.html'
     success_url = reverse_lazy('myrecipes')
