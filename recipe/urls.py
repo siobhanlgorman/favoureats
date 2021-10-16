e61e53de5953cdf4030270eaf04421ea98cccaa5
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-16 13:45:13 +0000

    Add view and url for myrecipes

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeList.as_view(), name='home'),
-    path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('about/', views.AboutPage.as_view(), name='about'),
+    path('recipes/', views.RecipeList.as_view(), name='recipes'),
+    path('myrecipes/', views.MyRecipeList.as_view(), name='myrecipes'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('favourite/<slug:slug>', views.RecipeFavourite.as_view(), name='recipe_favourite'),
 ]
