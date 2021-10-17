36a3a21337a8fbb3bdcd905f07f424c750e9bb9a
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-17 18:26:01 +0000

    Create recipe delete view and connect url and link in myrecipes.html

--- recipe/urls.py
+++ recipe/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('myrecipes/', views.MyRecipeList.as_view(), name='myrecipes'),
     path('recipecreate/', views.RecipeCreate.as_view(), name='recipecreate'),
     path('recipeedit/<slug:slug>', views.RecipeEdit.as_view(), name='recipeedit'),
+    path('recipedelete/<slug:slug>', views.RecipeDelete.as_view(), name='recipedelete'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('favourite/<slug:slug>', views.RecipeFavourite.as_view(), name='recipe_favourite'),
 ]
