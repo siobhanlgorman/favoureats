e1f72d772829bf452bb222b9a2370950ff7d8c76
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-21 11:43:38 +0000

    Amend function in admin.py to approve_reviews

--- recipe/admin.py
+++ recipe/admin.py
@@ -23,7 +23,7 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
+    actions = ['approve_reviews']
 
-    def approve_comments(self, request, queryset):
+    def approve_reviews(self, request, queryset):
         queryset.update(approved=True)
