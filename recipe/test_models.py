936e380acdaaf26d9103068c39782aaa2d61576e
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-25 19:40:14 +0000

    Start test for models

--- recipe/test_models.py
+++ recipe/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from .models import Recipe
+from .models import Recipe, Review
 
 # class TestModels(TestCase):
 #     def test_recipe_defaults_to_draft(self):
-#         recipe = Recipe.objects.create(name='Test Recipe')
-        
+#         recipe = Recipe.objects.create(title='Test Recipe')
+#         self.assertFalse(item.done)       
