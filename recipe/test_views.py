1db2f8e65aa99573e5159c654bcae9d7e6af5fc7
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-26 19:28:23 +0000

    Ad set up to test_views.py

--- recipe/test_views.py
+++ recipe/test_views.py
@@ -1,7 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from recipe.models import Recipe
+
 
 
 class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
 
     def test_get_homelist(self):
         response = self.client.get('/')
@@ -17,4 +22,16 @@ class TestViews(TestCase):
         response = self.client.get('/recipes/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'recipes.html')
+
+    # def test_get_recipe_create_page(self):
+    #     recipe = Recipe.objects.create(title='test recipe')
+    #     response = self.client.get('/recipecreate/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'recipecreate.html')
+
+    # def test_get_recipe_detail_redirects_nonregistered_users(self):
+    #     response = self.client.get('self.recipe_detail_url')
+    #     self.assertRedirects(response.status_code, 302)
+    
+
 
