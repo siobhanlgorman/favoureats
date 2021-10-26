15ce3225dc5ea664ba768208dbee153a82f000b0
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-26 18:52:52 +0000

    Add three working views tests in test_views.py

--- recipe/test_views.py
+++ recipe/test_views.py
@@ -7,3 +7,14 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+    
+    def test_get_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+    
+    def test_get_recipes_page(self):
+        response = self.client.get('/recipes/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes.html')
+
