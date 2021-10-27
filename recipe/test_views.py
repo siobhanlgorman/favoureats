89f084c6a5ca63d72977e9b0af098c1c4f3a9ba1
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-27 13:40:06 +0000

    Add tests to test_views.py

--- recipe/test_views.py
+++ recipe/test_views.py
@@ -5,6 +5,7 @@ from recipe.models import Recipe, User
 
 User = get_user_model()
 
+
 class TestViews(TestCase):
 
     def setUp(self):
@@ -13,12 +14,15 @@ class TestViews(TestCase):
         # self.login = self.client.login(username='testuser', password='my_password')
         # self.recipe_1 = Recipe.objects.create(
         # )
-     
+
         # create a test user
         self.user_a = User(username='testuser', email='testuser@email.com')
         self.user_a.set_password('my_password')
         self.client.login(username='testuser', password='my_password')
         self.user_a.save()
+        user_b = User.objects.create_user('user_2', 'email@invalid.com', 'a_password')
+        self.user_b = user_b
+
 
         # create a test recipe
         self.recipe_1 = Recipe.objects.create(
@@ -26,10 +30,11 @@ class TestViews(TestCase):
             author=self.user_a,
         )
         self.recipe_detail_url = reverse('recipe_detail', args=['test-recipe'])
+        self.recipecreate_url = reverse('recipecreate')
 
     def test_user_count(self):
         user_count = User.objects.all().count()
-        self.assertEqual(user_count, 1)
+        self.assertEqual(user_count, 2)
 
     def test_recipe_count(self):
         recipe_count = Recipe.objects.all().count()
@@ -42,7 +47,7 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-    
+
     def test_get_about_page(self):
         """
         Test about page response is 200
@@ -50,7 +55,7 @@ class TestViews(TestCase):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about.html')
-    
+
     def test_get_recipes_page(self):
         """
         Test recipes page response is 200
@@ -63,34 +68,55 @@ class TestViews(TestCase):
         """
         Test logged in user can access recipe detail page
         """
-        user = self.user_a
-        recipe = self.recipe_1
+        # user = self.user_a
+        # recipe = self.recipe_1
         response = self.client.get(self.recipe_detail_url)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(self.recipe_1.slug, 'test-recipe')
-    
+        # self.assertEqual(self.recipe_1.slug, 'test-recipe')
+        # self.assertTemplateUsed(response, 'recipe_form.html')
+
     def test_logged_in_user_can_access_recipe_create_page(self):
         """
-        Test logged in user can access recipe detail page
+        Test logged in user is redirected to recipe create page
         """
-        user = self.user_a
-        recipe = self.recipe_1
-        response = self.client.get(self.recipe_create_url)
+        # user = self.user_a
+        # recipe = self.recipe_1
+        response = self.client.get('/recipecreate/')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(self.recipe_1.slug, 'test-recipe')
+        # self.assertTemplateUsed(response, 'recipe_form.html')
+        # self.assertEqual(self.recipe_1.slug, 'test-recipe')
     
- 
+    def test_logged_in_user_can_access_recipe_edit_page(self):
+        """
+        Test logged in user is redirected to recipe edit page
+        """
+        # user = self.user_a
+        # recipe = self.recipe_1
+        response = self.client.get('/recipeedit/')
+        self.assertEqual(response.status_code, 302)
+        # self.assertTemplateUsed(response, 'recipe_form.html')
+        # self.assertEqual(self.recipe_1.slug, 'test-recipe')
+    
+    def test_logged_in_user_can_access_recipe_delete_page(self):
+        """
+        Test logged in user is redirected to recipe delete page
+        """
+        # user = self.user_a
+        # recipe = self.recipe_1
+        response = self.client.get('/recipedelete/')
+        self.assertEqual(response.status_code, 302)
+        # self.assertTemplateUsed(response, 'recipe_form.html')
+        # self.assertEqual(self.recipe_1.slug, 'test-recipe')
 
-
-    # def test_get_recipe_create_page(self):
+    # def test_invalid_request(self):
     #     """
-    #     Test logged in user can access recipe create and page response is 200
+    #     Test if logged in client can create a recipe
     #     """
-    #     recipe = 'test recipe'
-    #     user = self.user_a
-    #     response = self.client.get('/recipecreate/')
+    #     response = self.client.recipe()
+    #     self.client.login(username=self.user_b.username, password='a_password')
+    #     self.client.recipe("recipecreate", {"title": "this is a valid test"})
     #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'recipecreate.html')
+
 
 
     # def test_user_can_submit_recipe_create_form
@@ -101,10 +127,5 @@ class TestViews(TestCase):
     #     response = self.client.get('self.recipe_detail_url')
     #     self.assertRedirects(response.status_code, 302)
 
-
-
     # def test_get_recipe_edit_page(self)
     # def test_get_recipe_delete_page(self)
-    
-
-
