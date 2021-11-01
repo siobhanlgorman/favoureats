29d72ab1812ddb1b98e9a4e17430ab7a5ba6c9b7
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-01 08:03:42 +0000

    Deployment commit

--- favoureats/urls.py
+++ favoureats/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipe.urls'), name='recipe_urls'),
     path('accounts/', include('allauth.urls')),
-
 ]
 
 handler404 = 'favoureats.views.handler404'
